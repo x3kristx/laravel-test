aeea9399e9ead942960b180e12a3fc2a339cf1db
x3kristx <kristo.lember2@gmail.com>
2025-09-03 14:25:24 +0300

    test route tere

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,10 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get("/test", function() {
+    return "tere";
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
